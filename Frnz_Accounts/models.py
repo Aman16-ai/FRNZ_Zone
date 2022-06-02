ceeeeb81935627bbfddea9ec20a02e0af7e3fb4c
nandanv99 <nandanv99@gmail.com>
2022-06-02 15:39:31 +0530

    added one to one field not migrated ! anything yet .....

--- Frnz_Accounts/models.py
+++ Frnz_Accounts/models.py
@@ -1,5 +1,6 @@
 import email
 from operator import le
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,8 +8,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class user(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class user_profile(models.Model):
+    user=models.OneToOneField(User,on_delete=CASCADE,default=0)
     user_id = models.AutoField
     fullname = models.CharField(max_length=200)
     email = models.EmailField()
